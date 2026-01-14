2a4931e4be322f1fe3d7a8d9833505f8aebd5053
Luiz Hondo <120063146+LuizHondo@users.noreply.github.com>
2026-01-14 00:36:24 -0300

    chore: component proprieties added and inherited by the component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,12 +3,15 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 
+
+
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
       <StatusBar style="auto" />
-      <Button/>
+
+      <Button title="Entrar" activeOpacity={0.8} />
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -6,8 +6,8 @@ export const styles = StyleSheet.create({
     backgroundColor: '#d0d2d8',
     alignItems: 'center',
     paddingTop:62,
-    padding:24
-
+    padding:24,
+    gap: 10
   },
   text: {
     fontSize: 32,
