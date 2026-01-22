16f9e6bb4a783e4667d38a19c784ff35c14c0f3d
Luiz Hondo <120063146+LuizHondo@users.noreply.github.com>
2026-01-22 16:09:06 -0300

    fix: refactored Flatlist proprieties

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
@@ -8,7 +8,13 @@ import { FilterStatus } from '@/types/FilterStatus';
 import { Item } from '@/components/Item';
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
-
+// const ITEMS = Array.from({length: 50}).map((value, index) => String(index));
+const ITEMS = [
+  {id: "1", status: FilterStatus.DONE, description: "banana"},
+  {id: "2", status: FilterStatus.DONE, description: "alicate"},
+  {id: "3", status: FilterStatus.PENDING, description: "guaraná"},
+  {id: "4", status: FilterStatus.DONE, description: "Tijolo"},
+]
 
 
 export function Home() {
@@ -33,18 +39,32 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-        <ScrollView>
 
-          {Array.from({length: 50}).map((value, index)=>(
+
+
+
+          {/* {ITEMS.map((value)=>(
             <Item 
-            key={index}
-            data={{status: FilterStatus.DONE, description: "café"}}
-            onStatus={() => console.log("Alterna Status do index "+ index)}
-            onRemove={() => console.log("remover o index " + index)}
+              key={value}
+              data={{status: FilterStatus.DONE, description: String(value)}}
+              onStatus={() => console.log("Alterna Status do index ")}
+              onRemove={() => console.log("remover o index " )}
             
             />
-          ))}
-        </ScrollView>
+          ))} */}
+        <FlatList 
+          data={ITEMS} 
+          keyExtractor={item => item.id} 
+          renderItem={({item})=>(
+              <Item 
+                data={item}
+                onStatus={() => console.log("Alterna Status do index ")}
+                onRemove={() => console.log("remover o index " )}
+              
+              />
+            )
+          }
+        />
       </View>
 
       
